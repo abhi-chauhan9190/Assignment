7880be10b3143ea8133ac2de29fc5d93a85192df
Abhishek Chauhan <138496233+abhi-chauhan9190@users.noreply.github.com>
2024-07-09 20:00:01 +0530

    Updated readme file and deleted unit test file

---
+++ Player.java
@@ -0,0 +1,33 @@
+public class Player {
+    private String name;
+    private int health;
+    private int strength;
+    private int attack;
+
+    public Player(String name, int health, int strength, int attack) {
+        this.name = name;
+        this.health = health;
+        this.strength = strength;
+        this.attack = attack;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Assignment
+Magical Arena
+
+This project implements a simple Magical Arena simulation in Java.
+
+Running the Code
+
+Compile: Open a terminal or command prompt and navigate to the directory containing the Java files. Compile them using:
+                    javac MagicalArena.java Player.java MagicalArenaTest.java
+
+Run: Once compiled, execute the program with:
+                    java MagicalArena
+
+This will simulate a fight between two predefined players and display the results.
+
